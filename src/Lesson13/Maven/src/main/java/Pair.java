7b2b4ef229fa7ccaac5d08aad688c78db28ca832
artem <artemlushnya@gmail.com>
2022-07-01 19:28:17 +0300

    Lesson13

--- src/Lesson13/Maven/src/main/java/Pair.java
+++ src/Lesson13/Maven/src/main/java/Pair.java
@@ -3,16 +3,19 @@ import java.util.Locale;
 public class Pair {
     private final String value;
 
-
     public Pair(String value) {
         this.value = value;
+    }
+
+    private String toUpper() {
+        return value.toUpperCase();
     }
 
     @Override
     public String toString() {
         return "Pair{" +
                 value + ":" +
-                value.toUpperCase() +
+                toUpper() +
                 '}';
     }
 }
